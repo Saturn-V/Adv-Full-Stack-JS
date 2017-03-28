d5a9c5f3b1a5801bb0ffe90477c72ed074dac297
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-03-28 14:05:00 -0700

    [UPDATE] ...

--- 1. Diagnostic Assessment & Vanilla JavaScript 1 - DOM/baseline.js
+++ 1. Diagnostic Assessment & Vanilla JavaScript 1 - DOM/baseline.js
@@ -1,19 +1,42 @@
 // `$('#todos')`
-
+function $(elementId) {
+  document.getElementById(elementId)
+}
 
 // `.text()`
-
+function text(element, content) {
+  content = content || false
+  if content {
+    element.innerHTML = element.innerHTML + content
+  } else {
+    return element.textContent
+  }
+}
 
 // `.text("hello!")`
-
+// Above ^^^
 
 // `.val()`
-
+function val(element) {
+  return element.value
+}
 
 // `.prepend()`
-
+function prepend(item, element) {
+  element.insertBefore(item, element.childNodes[0])
+}
 
 // `.addClass('bold')`
-
+function addClass(className, element) {
+  var newClassName = ' ' + className
+  element.className += element.className ? newClassName : className
+}
 
 // $(document).ready()
+function documentReady() {
+  if (document.readyState() === "complete") {
+    return true
+  } else {
+    return false
+  }
+}
